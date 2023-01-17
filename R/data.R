619b30fa6a90adab087b08048357fc4d32e03f47
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2023-01-17 14:02:06 -0500

    Add SPF links and data descriptions for UNEMP TBILL RGDP PGDP #4

--- R/data.R
+++ R/data.R
@@ -15,9 +15,9 @@
 #' @source Simulated data.
 "mikedata"
 
-#' Civilian Unemployment Rate
+#' Civilian Unemployment Rate (UNEMP)
 #'
-#' Error Statistics for the Survey of Professional Forecasters for Nominal GNP/GDP
+#' Error Statistics for the Survey of Professional Forecasters for Unemployment Rate
 #'
 #' @format ## `UNEMP`
 #' A data frame with 219 rows and 31 columns:
@@ -29,3 +29,48 @@
 #' }
 #' @source <https://www.philadelphiafed.org/-/media/frbp/assets/surveys-and-data/survey-of-professional-forecasters/data-files/unemp/data_spf_error_statistics_unemp_1_aic.xls?la=en&hash=4CAD0B11FEAB6C4D0F30C38965FE3354>
 "UNEMP"
+
+#' 3-Month Treasury Bill Rate (TBILL)
+#'
+#' Error Statistics for the Survey of Professional Forecasters for Treasury Bill Rate (Three Month)
+#'
+#' @format ## `TBILL`
+#' A data frame with 219 rows and 31 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.philadelphiafed.org/-/media/frbp/assets/surveys-and-data/survey-of-professional-forecasters/data-files/tbill/data_spf_error_statistics_tbill_1_aic.xls?la=en&hash=F432350F84B0E4CCE9A1E9D399447CA9>
+"TBILL"
+
+#' Real Gross National Product/Gross Domestic Product (RGDP)
+#'
+#' Error Statistics for the Survey of Professional Forecasters for Real GNP/GDP
+#'
+#' @format ## `RDGP`
+#' A data frame with 219 rows and 31 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.philadelphiafed.org/-/media/frbp/assets/surveys-and-data/survey-of-professional-forecasters/data-files/rgdp/data_spf_error_statistics_rgdp_3_aic.xls?la=en&hash=3AC9E2D8A5299F93CA7E16CFAA974C22>
+"RGDP"
+
+#' Price Index for Gross National Product/Gross Domestic Product (PGDP)
+#'
+#' Error Statistics for the Survey of Professional Forecasters for GNP/GDP Price Index
+#'
+#' @format ## `PGDP`
+#' A data frame with 219 rows and 31 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.philadelphiafed.org/-/media/frbp/assets/surveys-and-data/survey-of-professional-forecasters/data-files/pgdp/data_spf_error_statistics_pgdp_3_aic.xls?la=en&hash=148987D03D54DA5391A44F28CBC12729>
+"PGDP"
