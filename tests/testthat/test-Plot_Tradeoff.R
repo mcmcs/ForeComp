241136ffd91fc5dbde754fc6565dffe4c1feb677
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2023-07-17 16:27:39 -0400

    Include tests for testing plotting M labels #15

--- tests/testthat/test-Plot_Tradeoff.R
+++ tests/testthat/test-Plot_Tradeoff.R
@@ -24,3 +24,32 @@ test_that("Check the dimension of the output.", {
   expect_true(nrow(output[[2]]) == length(m_set));
 
 })
+
+# =====================================================
+# Test 3
+test_that("Check that the M labels are plotted by default.", {
+
+  output = Plot_Tradeoff(data = TBILL,
+                f1   = "SPFfor_Step1",
+                f2   = "NCfor_Step1",
+                y    = "Realiz1",
+                );
+
+  expect_false(is.null(output[[1]]$layers[[3]]$computed_mapping$label))
+
+})
+
+# =====================================================
+# Test 4
+test_that("Check points have no labels when no_m_label = TRUE.", {
+
+  output = Plot_Tradeoff(data = TBILL,
+                f1   = "SPFfor_Step1",
+                f2   = "NCfor_Step1",
+                y    = "Realiz1",
+                no_m_label = TRUE
+                );
+
+  expect_error(is.null(output[[1]]$layers[[3]]$computed_mapping$label))
+
+})
